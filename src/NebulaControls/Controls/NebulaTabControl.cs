73cead534a34985afc39a67a4cb44b3f9ec19cda
DominiqueChalle <dominique.challe@gmail.com>
2026-07-08 15:25:38 +0200

    Refine tab control binder depth

--- src/NebulaControls/Controls/NebulaTabControl.cs
+++ src/NebulaControls/Controls/NebulaTabControl.cs
@@ -1,13 +1,68 @@
 // Nom: NebulaTabControl
-// Version: V1.02
-// Description: TabControl and TabItem base controls styled for Nebula top tab navigation.
+// Version: V1.04
+// Description: TabControl and TabItem base controls styled for Nebula top tab navigation with binder-style depth.
 
+using System.Collections.Specialized;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
 
 namespace NebulaControls.Controls;
 
 public class NebulaTabControl : TabControl
 {
+    protected override DependencyObject GetContainerForItemOverride()
+    {
+        return new NebulaTabItem();
+    }
+
+    protected override bool IsItemItsOwnContainerOverride(object item)
+    {
+        return item is NebulaTabItem;
+    }
+
+    protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+    {
+        base.OnItemsChanged(e);
+        UpdateTabDepthAsync();
+    }
+
+    protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+    {
+        base.OnSelectionChanged(e);
+        UpdateTabDepth();
+    }
+
+    protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+    {
+        base.PrepareContainerForItemOverride(element, item);
+        UpdateTabDepthAsync();
+    }
+
+    private void UpdateTabDepthAsync()
+    {
+        Dispatcher.BeginInvoke(UpdateTabDepth, DispatcherPriority.Loaded);
+    }
+
+    private void UpdateTabDepth()
+    {
+        var count = Items.Count;
+
+        for (var index = 0; index < count; index++)
+        {
+            if (ItemContainerGenerator.ContainerFromIndex(index) is not TabItem tabItem)
+            {
+                continue;
+            }
+
+            var depth = tabItem.IsSelected
+                ? count + 10
+                : count - index;
+
+            Panel.SetZIndex(tabItem, depth);
+        }
+    }
 }
 
 public class NebulaTabItem : TabItem
